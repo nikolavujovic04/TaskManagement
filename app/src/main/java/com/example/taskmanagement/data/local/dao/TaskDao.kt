89f1e1133da07ceb142c784885eb725c64e198cb
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-03-18 16:59:20 +0100

    Kreirana funkcionalnost za dobijanje danasnjih taskova

--- app/src/main/java/com/example/taskmanagement/data/local/dao/TaskDao.kt
+++ app/src/main/java/com/example/taskmanagement/data/local/dao/TaskDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Query
 import com.example.taskmanagement.data.local.models.Task
 import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
 
 @Dao
 interface TaskDao {
@@ -13,4 +14,10 @@ interface TaskDao {
 
     @Query("SELECT * FROM tasks ORDER BY isCompleted ASC, dueDate DESC")
     fun getAllTasks(): Flow<List<Task>>//vracamo listu taskova
+
+    @Query("SELECT * FROM tasks WHERE dueDate= :today AND isCompleted = 0")
+    fun getTasksDueToday(today: LocalDate): Flow<List<Task>>
+
+    @Query("SELECT COUNT() WHERE isCompleted = 1")
+    fun getCompletedCount(): Flow<Int>
 }
