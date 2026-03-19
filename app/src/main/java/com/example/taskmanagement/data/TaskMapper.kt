4516c3ec5f4aad1d11c6470efa9c97fb63f1dc21
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-03-19 11:23:44 +0100

    Omogucena komunikacija sa serverom

---
+++ app/src/main/java/com/example/taskmanagement/data/TaskMapper.kt
@@ -0,0 +1,43 @@
+package com.example.taskmanagement.data
+
+import com.example.taskmanagement.data.local.models.SyncStatus
+import com.example.taskmanagement.data.local.models.Task
+import com.example.taskmanagement.data.remote.models.TaskDto
+import com.example.taskmanagement.data.remote.models.TaskDtoItem
+import java.time.LocalDate
+
+object TaskMapper {
+    //ovde konvertujemo podatke sa servera u nas entitet, tacnije task
+    fun mapDtoToEntity(dto: TaskDto): List<Task>{
+        return dto.map { dtoItem ->
+            val safeDueDate = try{
+                LocalDate.parse(dtoItem.dueDate)
+            }catch (e: Exception){
+                LocalDate.now()
+            }
+            Task(
+                remoteId = dtoItem.id,
+                title = dtoItem.title,
+                description = dtoItem.description,
+                priority = dtoItem.priority,
+                reminderEnabled = dtoItem.reminderEnabled,
+                dueDate = safeDueDate,
+                tags = dtoItem.tags,
+                isCompleted = dtoItem.isCompleted,
+                syncStatus = SyncStatus.SYNCED //kada preuzimamo sa servera pretpostavljamo da su podaci vec sinhronizaovani
+            )
+        }
+    }
+    fun mapEntityToDto(entity: Task): TaskDtoItem{
+        return TaskDtoItem(
+            id = if(entity.remoteId.isNotBlank()) entity.remoteId.toInt() else 0,
+            title = entity.title,
+            description = entity.description,
+            priority = entity.priority,
+            reminderEnabled = entity.reminderEnabled,
+            dueDate = entity.dueDate.toString(), //Convert LocalDate to a standard "YYYY-MM-DD" string
+            tags = entity.tags,
+            isCompleted = entity.isCompleted,
+        )
+    }
+}
