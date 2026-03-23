4d0db4f57bfb4a2342d8d0523e5ff0262a578729
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-03-23 21:04:14 +0100

    Kreirana komponenta TaskItem

--- app/src/main/java/com/example/taskmanagement/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/taskmanagement/data/local/AppDatabase.kt
@@ -14,16 +14,17 @@ import com.example.taskmanagement.data.local.type_converters.DateTypeConverter
 abstract class AppDatabase: RoomDatabase() {
     abstract fun taskDao(): TaskDao
 
+    //Singleton
     companion object{ //pravljenje jedne jedine instance baze, jedna globalna instanca
         //jer ukoliko bih stalno kreirao bilo bi sporije, kresovalo bi itd
-        @Volatile//ovo znaci da je ona promenljiva
+        @Volatile//ovo znaci da je ona vidljiva drugim nitima
         private var INSTANCE: AppDatabase? = null
 
         fun getDatabase(context: Context): AppDatabase{ //kreira i vraca bazu podataka
             return INSTANCE ?: synchronized(this){ //zakljucavamo sinhronizaciju da bi izbegli probleme sa vise niti,
                 //zakljucavamo da se ne poziva vise od jednom
                 //sprecava da 2 trhread-a naprave bazu u isto veme
-                val instance = Room.databaseBuilder(
+                val instance = Room.databaseBuilder( //ako je INSTANCE null onda kreiramo novu instance
                     context = context.applicationContext,
                     AppDatabase::class.java,
                     name = "task_database"//ime baze
