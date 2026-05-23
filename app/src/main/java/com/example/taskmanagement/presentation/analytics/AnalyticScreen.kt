a5fc97e269263ba80ec6cc0fb4995b89da1f50f8
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-05-23 22:48:49 +0200

    Dizajniran prikaz izvrsenih taskova

--- app/src/main/java/com/example/taskmanagement/presentation/analytics/AnalyticScreen.kt
+++ app/src/main/java/com/example/taskmanagement/presentation/analytics/AnalyticScreen.kt
@@ -1,14 +1,26 @@
 package com.example.taskmanagement.presentation.analytics
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
@@ -39,6 +51,52 @@ private fun AnalyticsScreen(
             contentAlignment = Alignment.Center
         ){
             Text(text = "No task data for this month")
+        }
+        return
+    }
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+    ) {
+        Text(
+            text = "Overview",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold
+        )
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            colors = CardDefaults.cardColors(MaterialTheme.colorScheme.surfaceVariant) ,
+            shape = MaterialTheme.shapes.medium,
+            elevation = CardDefaults.cardElevation()
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = "Tasks Completed",
+                    style = MaterialTheme.typography.displaySmall,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+                Row(
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Text(
+                        text = state.completedTasksCount.toString(),
+                        style = MaterialTheme.typography.displaySmall,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                    Spacer(Modifier.width(8.dp))
+                    Text(
+                        text = "This Month",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(.7f)
+                    )
+                    Spacer(Modifier.height(16.dp))
+                }
+            }
         }
     }
 }
